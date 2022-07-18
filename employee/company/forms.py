4c078a4935a60a46c0888e6a49cde2b9617aaf87
Shirleen19 <gathungushirleen@gmail.com>
2022-07-18 13:15:58 +0300

    first commit

---
+++ employee/company/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from company.models import Employee
+from company.models import Company
+
+# This is for employee
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = "__all__"
+
+#this is for company
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = "__all__"
